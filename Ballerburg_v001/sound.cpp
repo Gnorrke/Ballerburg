e7339eb05d4a04c9e73bd97cc43237a734617444
Gnorrke <Max.Niederauer@gmail.com>
2014-11-26 00:52:33 +0100

    clean up
    title

--- Ballerburg_v001/sound.cpp
+++ Ballerburg_v001/sound.cpp
@@ -5,8 +5,8 @@
 Sound::Sound()
 {
     if (Mix_OpenAudio( 44000, MIX_DEFAULT_FORMAT, 2, 4096 ) == -1) std::cout << "Fehler" << std::endl;
-    backgroundMusic = Mix_LoadMUS("sounds/theme.mp3");
-    Mix_PlayMusic( backgroundMusic, -1 );
+    backgroundMusic = Mix_LoadMUS("theme.mp3");
+    Mix_FadeInMusic(backgroundMusic, 10, 2000 );
 }
 
 Sound::~Sound()
